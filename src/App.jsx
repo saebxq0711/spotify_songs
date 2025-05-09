893ee6e4b762f2b1a9f7b255b2302e9288851ada
Sebastian A <jsebaslozano2006@gmail.com>
2025-05-09 08:56:20 -0500

    spotify

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-const CLIENT_ID = "67b5d61bcd214dd380fa7537d81b1ffa";
-const REDIRECT_URI = "https://spotify-love-songs.netlify.app";
+const CLIENT_ID = "67b5d61bcd214dd380fa7537d81b1ffa";  // tu Client ID
+const REDIRECT_URI = "https://spotify-love-songs.netlify.app/dashboard";  // tu Redirect URI
 const SCOPES = ["user-library-read", "playlist-modify-private", "user-top-read"];
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${SCOPES.join('%20')}`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${SCOPES.join('%20')}&show_dialog=true`;
+
 
 function App() {
   return (
